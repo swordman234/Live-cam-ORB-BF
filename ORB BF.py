2a140cdc0fb400c315992a370fd9a950b5934ea4
swordman234 <alexwibisono18@gmail.com>
2021-03-24 04:22:09 +0700

    init

---
+++ ORB BF.py
@@ -0,0 +1,66 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+MIN_MATCH_COUNT = 10
+
+img1 = cv2.imread('test1/box.png')          # queryImage
+img2 = cv2.imread('test1/box_in_scene.png') # trainImage
+
+# Initiate ORB detector
+orb = cv2.ORB_create(1000, 1.2)
+
+# find the keypoints and descriptors with ORB
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
+
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+
+# Match descriptors.
+matches = bf.knnMatch(des1,des2, k=2)
+
+# store all the good matches as per Lowe's ratio test.
+good = []
+for m,n in matches:
+    if m.distance < 0.7*n.distance:
+        good.append(m)
+
+if len(good)>MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+    matchesMask = mask.ravel().tolist()
+    h,w,d = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,M)
+    box = np.array(dst, dtype="int")
+    (tl, bl, br, tr) = box
+    midpoint = (tl+bl+br+tr)/4
+    print( "pst = ({})".format(pts) )
+    print( "dst = ({})".format(dst) )
+    print( "box = ({})".format(box) )
+    print( "top left = ({})".format(tl) )
+    print( "bottom left = ({})".format(bl) )
+    print( "top right = ({})".format(tr) )
+    print( "bottom right = ({})".format(br) )
+    print( "midpoint 0 = ({})".format(midpoint[0][0]) )
+    print( "midpoint 1 = ({})".format(midpoint[0][1]) )    
+
+    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+    img2 = cv2.circle(img2,(int(midpoint[0][0]),int(midpoint[0][1])),5,255,-1)
+else:
+    print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+    matchesMask = None
+
+
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+
+plt.imshow(img3, 'gray'),plt.show()
+plt.imshow(img2, 'gray'),plt.show()
---
+++ v0_1 BF.py
@@ -0,0 +1,95 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+#import time
+
+cap = cv2.VideoCapture(0)
+#set the resolution
+width =  640 #640 1280
+height =  480 #480 720
+
+#changing the resolution
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
+
+MIN_MATCH_COUNT = 10
+
+#input the reference image
+img = cv2.imread('test1/raspberry pi.jpg')
+
+# create BFMatcher object
+bf  = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+
+
+def ORB_module (cam_image,input_image):
+    img1 = cam_image
+    img2 = input_image
+
+    #change the camera image and input image to black and white
+    img1 = cv2.cvtColor(cam_image, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+
+    # Initiate ORB detector with max 1000 features and scaling 1.2
+    orb = cv2.ORB_create(1000, 1.2)
+
+    #search the keypoint
+    kp1, des1 = orb.detectAndCompute(img1,None)
+    kp2, des2 = orb.detectAndCompute(img2,None)
+
+    # Match descriptors.
+    matches = bf.knnMatch(des1,des2,k=2)
+
+    # Apply ratio test
+    good = []
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            good.append(m)
+
+    if len(good)>MIN_MATCH_COUNT:
+        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+        matchesMask = mask.ravel().tolist()
+        h,w = img1.shape 
+        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        dst = cv2.perspectiveTransform(pts,M)
+        (tl, tr, br, bl) = dst
+        midpoint = (tl+bl+br+tr)/4
+
+        input_image = cv2.polylines(input_image,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+        input_image = cv2.circle(input_image,(int(midpoint[0][0]),int(midpoint[0][1])),5,255,-1)
+        
+        #for img3 (debugging with black and white)
+        #img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+    else:
+        print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+        matchesMask = None
+
+    #Draw matches. (for debugging)
+    #draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+    #               singlePointColor = None,
+    #               matchesMask = matchesMask, # draw only inliers
+    #               flags = 2)
+
+    #img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+    #cv2.imshow("ORB Keypoints and matched", img3)
+
+    cv2.imshow("the actual frame", input_image)
+
+    return len(matches)
+
+while(True):
+    ret, frame= cap.read()
+   
+    matches = ORB_module(img,frame)
+    
+    #press enter to exit
+    if cv2.waitKey(1) == 13:
+        break
+    #time.sleep(0.05)
+
+
+
+
+cap.release()
+cv2.destroyAllWindows()
